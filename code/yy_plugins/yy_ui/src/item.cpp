bda6b3ab2a2a98ec356254a93bfa03ac1fb8e762
ZHANG Junliang (CM/ESW2-CN) <zujiszh@bosch.com>
2019-02-19 17:01:39 +0800

    ui module use the hierarchy

---
+++ code/yy_plugins/yy_ui/src/item.cpp
@@ -0,0 +1,227 @@
+#include "item.h"
+//#include "anchor_fill_component.h"
+#include "yy_ui/inc/i_gui.h"
+
+//NS_YY_BEGIN
+Item::Item()
+	:m_top(0.0f),m_left(0.0f),m_width(0.0f),m_height(0.0f),m_opacity(1),m_visible(false),m_bTransformChanged(false)
+{}
+
+
+void Item::OnCreate(const VariantMap& args)
+{
+	Entity::OnCreate(args);
+
+	//AddComponent("AnchorFillComponent");
+	//m_pAnchorLayout = AddComponent("AnchorLayoutComponent");
+}
+
+void Item::OnDestroyed()
+{
+	Entity::OnDestroy();
+}
+ 
+ void Item::Show()
+ {
+ 	YY_OBJECTID id = IGUI::Instance()->GetDesktopForm();
+ 	Entity* pDeskTop = (Entity*)GetMgr()->Find(id);
+ 	if (NULL == pDeskTop)
+ 	{
+ 		return;
+ 	}
+ 
+ 	pDeskTop->AddChild(this);
+ }
+
+ void Item::Close()
+ {
+	 YY_OBJECTID id = IGUI::Instance()->GetDesktopForm();
+	 Entity* pDeskTop = (Entity*)GetMgr()->Find(id);
+	 if (NULL == pDeskTop)
+	 {
+		 return;
+	 }
+
+	 pDeskTop->RemoveChild(this);
+ }
+
+ void Item::ShowModal()
+ {
+	 IGUI::Instance()->AddModalForm(GetID());
+ }
+ 
+ void Item::CloseModal()
+ {
+ 	IGUI::Instance()->RemoveModalForm(GetID());
+ }
+
+float Item::GetTop()
+{
+	return m_top; 
+}
+
+Item* Item::FindParentTransform()
+{
+	Entity* pParent = FindParent();
+	if (nullptr == pParent)
+		return nullptr;
+
+	Component* pComponent = pParent->FindComponent("Item");
+	if (!pComponent)
+		return nullptr;
+
+	return (Item*)pComponent;
+}
+
+float Item::GetAbsTop()
+{
+	Item* pParent = FindParentTransform();
+	if (nullptr == pParent)
+		return GetTop();
+
+	float nParentTop = pParent->GetAbsTop();
+	return nParentTop + GetTop();
+}
+
+float Item::GetLeft()
+{
+	return m_left; 
+}
+
+float Item::GetAbsLeft()
+{
+	Item* pParent = FindParentTransform();
+	if (nullptr == pParent)
+		return GetLeft();
+
+	float nParentLeft = pParent->GetAbsLeft();
+	return nParentLeft + GetLeft();
+}
+
+void Item::SetTop(float top)
+{
+	m_top = top;
+	SetTransformChanged(); 
+}
+
+
+void Item::SetAbsTop(float top)
+{
+	Item* pParent = FindParentTransform();
+	if (nullptr == pParent)
+	{
+		SetTop(top);
+		return;
+	}
+
+	float nParentTop = pParent->GetAbsTop();
+	SetTop(top - nParentTop);
+}
+
+void Item::SetLeft(float left)
+{
+	m_left = left;
+	SetTransformChanged();
+}
+
+void Item::SetAbsLeft(float left)
+{
+	Item* pParent = FindParentTransform();
+	if (nullptr == pParent)
+	{
+		SetLeft(left);
+		return;
+	}
+
+	float nParentLeft = pParent->GetAbsLeft();
+	SetLeft(left-nParentLeft);
+}
+
+float Item::GetWidth()
+{
+	return m_width; 
+}
+
+float Item::GetHeight()
+{
+	return m_height; 
+}
+
+void Item::SetWidth(float w) 
+{
+	m_width = w;
+	SetTransformChanged();
+}
+
+void Item::SetHeight(float h)
+{
+	m_height = h;
+	SetTransformChanged();
+}
+
+int Item::GetOpacity()
+{
+	return m_opacity; 
+}
+
+void Item::SetOpacity(int opacity)
+{
+	m_opacity = opacity;
+}
+
+bool Item::GetVisible()
+{
+	return m_visible; 
+}
+
+void Item::SetVisible(bool visible)
+{
+	m_visible = visible;
+	if (m_visible)
+		Show();
+	else
+		Close();
+}
+
+
+// void Item::OnAddBatch(IBatchGroup* pBatchGroup)
+// {
+// 	pBatchGroup->AddGUIBatch(this);
+// 
+// 	int nChildNum = GetChildCount();
+// 	for (int i = 0; i < nChildNum; i++)
+// 	{
+// 		YY_OBJECTID id = GetChildByIndex(i);
+// 		BaseObject* pObject = GetMgr()->Find(id);
+// 		if (nullptr == pObject)
+// 			continue;
+// 
+// 		if (!pObject->IsInstanceOf("Item"))
+// 			continue;
+// 
+// 		Item* pItem = (Item*)pObject;
+// 		pItem->OnAddBatch(pBatchGroup);
+// 	}
+// }
+
+
+// void Item::OnRenderAfter(RenderContext* pCxt)
+// {
+// 	Entity::OnRenderAfter(pCxt);
+// 	m_bTransformChanged = false;
+// }
+
+void Item::SetTransformChanged(bool bChanged)
+{
+	m_bTransformChanged = bChanged;
+}
+
+
+Item* ItemComponent::FindItem()
+{
+	if (!GetOwner()->IsInstanceOf("Item"))
+		return nullptr;
+	return (Item*)GetOwner();
+}
+
+//NS_YY_END
---
+++ code/yy_plugins/yy_ui/src/item.h
@@ -0,0 +1,99 @@
+/************************************************************************/
+/*
+@author:  junliang
+@brief:   
+@time:    2018/09/28
+*/
+/************************************************************************/
+#pragma once
+
+#include "core/inc/yy_entity.h"
+#include "yy_render/inc/i_batch.h"
+
+
+// texture offset
+//#define SETTEXX(x) ((float(x) - 0.5f)) 
+//#define SETTEXY(y) (-(float(y) -0.5f))
+
+// point offset, normally, (0,0) is in the right,left, so need to change to top,left
+//#define SETX(x)	((x-0.5f))
+//#define SETY(y)	(-(y-0.5f))
+
+
+//#define COLOR_ARGB(a, r, g, b) ((unsigned int)((((a) & 0xFF)<<24) \
+//	| (((r) & 0xFF) << 16) | (((g) & 0xFF) << 8) | ((b) & 0xFF)))
+
+//#define COLOR_RGBA(r, g, b, a) COLOR_ARGB(a, r, g, b)
+
+// color: a r g b
+#define COLOR_RED 0xFFFF0000
+#define COLOR_GREE 0xFF00FF00
+#define COLOR_BLUE 0xFF0000FF
+#define COLOR_EDITOR 0xFF535353
+#define COLOR_TREEITEM_HIGHLIGHT 0xFF3A3A3A
+
+//NS_YY_BEGIN
+class Item : public YY::Entity
+{
+	YY_BEGIN(Item, Entity);
+	YY_FIELD(&Item::m_top, "top", "");
+	YY_FIELD(&Item::m_left, "left", "");
+	YY_FIELD(&Item::m_width, "width", "");
+	YY_FIELD(&Item::m_height, "height", "");
+	//YY_FIELD(&Item::m_pAnchorLayout, "anchor", "");
+	YY_END
+public:
+	Item();
+	virtual void OnCreate(const VariantMap& args);
+	virtual void OnDestroyed();
+
+
+	void SetLeft(float left);
+	void SetAbsLeft(float left);
+	float GetLeft();
+	float GetAbsLeft();
+
+	void SetTop(float top);
+	void SetAbsTop(float top);
+	float GetTop();
+	float GetAbsTop();
+	Item* FindParentTransform();
+
+
+	float GetWidth();
+	void SetWidth(float w);
+
+	float GetHeight();
+	void SetHeight(float h);
+
+	int GetOpacity();
+	void SetOpacity(int opacity);
+	bool GetVisible();
+	void SetVisible(bool visible);
+
+	void SetTransformChanged(bool bChanged=true);
+	bool IsTransformChanged() { return m_bTransformChanged; }
+private:
+	void Show();
+	void Close();
+	void ShowModal();
+	void CloseModal();
+private:
+	float m_top;
+	float m_left;
+	float m_width;
+	float m_height;
+	int m_opacity;
+	bool m_visible;
+	bool m_bTransformChanged;
+	//Component* m_pAnchorLayout;
+};
+
+class ItemComponent : public RenderComponent
+{
+	YY_BEGIN(ItemComponent, RenderComponent);
+	YY_END
+public:
+	Item* FindItem();
+};
+//NS_YY_END
